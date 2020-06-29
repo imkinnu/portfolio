c4ea2f24d4b71d848fbf9299c239665fbb02c228
Kiran Tirumlasetti <50947441+imkinnu@users.noreply.github.com>
2020-06-29 08:01:03 +0530

    Updates

---
+++ precache-manifest.ec3163deac1ebbc02a133c59ca40103e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9438ffd8ad66d3dab63f2b9de9db3e82",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2ba62282755bb8a75a0f",
+    "url": "/portfolio/static/css/main.2668268d.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba62282755bb8a75a0f",
+    "url": "/portfolio/static/js/main.85204dea.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
