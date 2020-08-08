53a67360caca0c9b56038bc029b345a1e7287e8a
Kiran Tirumlasetti <50947441+imkinnu@users.noreply.github.com>
2020-08-08 13:56:22 +0530

    Updates

---
+++ precache-manifest.a811158c40ebae0c8e07b0d99c2b7d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ce8d451f149b6796f6d14ba7ce58e7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f79ed5fb4c20e66b3afb",
+    "url": "/portfolio/static/css/main.0b60a75b.chunk.css"
+  },
+  {
+    "revision": "c8c58d6cd773703d3a55",
+    "url": "/portfolio/static/js/2.78910f1a.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/portfolio/static/js/2.78910f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79ed5fb4c20e66b3afb",
+    "url": "/portfolio/static/js/main.d0a9a731.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ec606c3e88cc4072a096d95298411af8.js"
+  "/portfolio/precache-manifest.a811158c40ebae0c8e07b0d99c2b7d7d.js"
 );
 
 self.addEventListener('message', (event) => {
