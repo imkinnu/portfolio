01a9e0184e09e2a10713972f0ffed447c512f4ab
Kiran Tirumlasetti <50947441+imkinnu@users.noreply.github.com>
2020-08-11 10:13:32 +0530

    modal header color added

--- src/components/ModalNotCompatible/ModalNotCompatible.js
+++ src/components/ModalNotCompatible/ModalNotCompatible.js
@@ -15,7 +15,7 @@ const ModalNotCompatible = ({ display }) => {
         <div class="modal" id="exampleModalCentered" tabindex="-1" style={{ display: `${styles}` }} role="dialog" aria-labelledby="exampleModalCenteredLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
+                    <div class="modal-header bg-primary text-white">
                         <h5 class="modal-title" id="exampleModalCenteredLabel">Information</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
